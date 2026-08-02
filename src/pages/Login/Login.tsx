fffabb88231918ca2596e0a856ee604bc9f3d1dd
Omar Cubas <omar.cubas.b@gmail.com>
2026-08-02 18:48:54 -0500

    fix: remove unused TextField import

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router";
 import { Controller, useForm } from "react-hook-form";
-import { Button, TextField } from "@mui/material";
+import { Button } from "@mui/material";
 import "./Login.scss";
 import type { loginForm } from "../../types/User";
 import { signIn } from "../../services/authService";
